2873cadfd3c4ab3acedcf13fa141efd009ff79aa
Schneider <cameron.schneider@champlain.edu>
2021-02-16 16:16:22 -0600

    Just adding comments

--- SimpleFPSTemplate-master/Source/FPSGame/Public/FPSCube.h
+++ SimpleFPSTemplate-master/Source/FPSGame/Public/FPSCube.h
@@ -1,4 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
+//Authored by Cameron Schneider & Dianna Cornett
 
 #pragma once
 
@@ -24,12 +25,15 @@ public:
 	// Sets default values for this actor's properties
 	AFPSCube();
 
+	//The mesh to display for the cube object
 	UPROPERTY(EditDefaultsOnly)
 	UStaticMeshComponent* CubeMesh;
 
+	//Maximum health of the cube
 	UPROPERTY(EditDefaultsOnly)
 	float MaxHealth = 100.0f;
 
+	//Current health of the cube, initialized to the max health by default
 	UPROPERTY(EditDefaultsOnly)
 	float CurrentHealth;
 
@@ -43,10 +47,11 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-	//called when the cube dies
+	//Respond to the death delegate invocation, can be overridden by subclasses
 	UFUNCTION()
 	virtual void DeathResponse();
 
+	// The actors to spawn on death
 	UPROPERTY(EditDefaultsOnly)
 	TSubclassOf<AFPSCube> SpawnedActor;
 	
@@ -57,6 +62,7 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	// Override of AActor::TakeDamage() for custom behavior
 	virtual float TakeDamage(float DamageTaken, struct FDamageEvent const& DamageEvent, class AController* EventInstigator, class AActor* DamageCauser) override;
 
 	
