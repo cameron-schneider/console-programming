201f7f2d4f853e23abb85d67e39477d80985ed6e
Lansingh Freeman <chicken.fries.are.good@gmail.com>
2021-03-09 10:33:57 -0500

    added in method of mass explosion

--- SimpleFPSTemplate-master/Source/FPSGame/Private/FPSBombActor.cpp
+++ SimpleFPSTemplate-master/Source/FPSGame/Private/FPSBombActor.cpp
@@ -2,6 +2,7 @@
 
 
 #include "FPSBombActor.h"
+#include "FPSCube.h"
 #include "Kismet/GameplayStatics.h"
 
 // Sets default values
@@ -32,6 +33,33 @@ void AFPSBombActor::Tick(float DeltaTime)
 void AFPSBombActor::Explode()
 {
 	UGameplayStatics::SpawnEmitterAtLocation(this, Explosion, GetActorLocation());
+
+	//Lance's Bomb Actor Method
+
+	FCollisionObjectQueryParams QueryParams;
+	QueryParams.AddObjectTypesToQuery(ECC_WorldDynamic);
+	QueryParams.AddObjectTypesToQuery(ECC_PhysicsBody);
+
+	FCollisionShape CollShape;
+	CollShape.SetSphere(500.0f);
+
+	TArray<FOverlapResult> OutOverlaps;
+	GetWorld()->OverlapMultiByObjectType(OutOverlaps, GetActorLocation(), FQuat::Identity, QueryParams, CollShape);
+
+	for (FOverlapResult Result : OutOverlaps)
+	{
+		UPrimitiveComponent* Overlap = Result.GetComponent();
+		if (Overlap && Overlap->IsSimulatingPhysics())
+		{
+			//GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Red, FString(Overlap->GetOwner()->GetClass()->GetSuperClass()->GetFName().ToString()));
+
+			if (Overlap->IsA<AFPSCube>)
+			{
+				Cast<AFPSCube>(Overlap->GetOwner())->TakeDamage();
+				Overlap->GetOwner()->Destroy();
+			}
+		}
+	}
 
 	Destroy();
 }
